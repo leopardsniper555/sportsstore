6e34751898790067f7deb29a52416d3310059fef
leopardsniper555@gmail.com <leopardsniper555>
2023-05-06 07:51:48 -0700

    sportsstore Rest

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ module.exports = function () {
     return { 
         categories: ["Watersports", "Soccer", "Chess"],
         products: [
-            { id: 1, name: "Kayak", category: "Watersports", 
+            { id: 1, name: "Kaak", category: "Watersports", 
                 description: "A boat for one person", price: 275 },
             { id: 2, name: "Lifejacket", category: "Watersports", 
                 description: "Protective and fashionable", price: 48.95 },
